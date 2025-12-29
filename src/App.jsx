c868e6baa0ef34d5096ef9dd52592961dd25c85a
Tomas Jara <tomasignaciojd@gmail.com>
2025-12-28 21:59:25 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { MRT_example_basic } from './MRT_example_basic'
 function App() {
   return (
     <>
-     
+     <h1 style={{fontFamily:'monospace'}}>Prueba de tabla <span style={{fontFamily:'monospace'}}>Material React Table</span></h1>
       <MRT_example_basic/>
     </>
   )
